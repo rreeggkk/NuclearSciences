d30a4b554f9ca55aa8451d69e1790c7ab651266b
rreeggkk <rreeggkk1@gmail.com>
2016-08-27 21:32:25 -0400

    temporary fix to prevent some crashes with mekanism until a bug is fixed

--- src/main/java/rreeggkk/nuclearsciences/common/util/CompatUtil.java
+++ src/main/java/rreeggkk/nuclearsciences/common/util/CompatUtil.java
@@ -62,6 +62,9 @@ public class CompatUtil {
 			TileEntity te = world.getTileEntity(pos.add(dir.getFrontOffsetX(), dir.getFrontOffsetY(), dir.getFrontOffsetZ()));
 			if (te != null && !te.isInvalid() && te.hasCapability(CapabilityUtil.CAPABILITY_CONSUMER, dir.getOpposite())) {
 				ITeslaConsumer cons = te.getCapability(CapabilityUtil.CAPABILITY_CONSUMER, dir.getOpposite());
+				if (cons == null) {
+					continue; //Why did this happen? This should not happen. The point of this is just to stop a crash for now
+				}
 				long power = cons.givePower(energy.getStored(), true);
 				if (power > 0) {
 					totalPower += power;

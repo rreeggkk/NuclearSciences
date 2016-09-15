c50bbca00823bc6165480b577f11413a72f466de
rreeggkk <rreeggkk1@gmail.com>
2016-09-15 17:54:18 -0400

    allows output into forge energy systems

--- src/main/java/rreeggkk/nuclearsciences/common/util/CapabilityUtil.java
+++ src/main/java/rreeggkk/nuclearsciences/common/util/CapabilityUtil.java
@@ -5,18 +5,22 @@ import net.darkhax.tesla.api.ITeslaHolder;
 import net.darkhax.tesla.api.ITeslaProducer;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.CapabilityInject;
+import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 
 public class CapabilityUtil {
     @CapabilityInject(ITeslaConsumer.class)
-    public static Capability<ITeslaConsumer> CAPABILITY_CONSUMER = null;
+    public static Capability<ITeslaConsumer> TESLA_CONSUMER = null;
     
     @CapabilityInject(ITeslaProducer.class)
-    public static Capability<ITeslaProducer> CAPABILITY_PRODUCER = null;
+    public static Capability<ITeslaProducer> TESLA_PRODUCER = null;
     
     @CapabilityInject(ITeslaHolder.class)
-    public static Capability<ITeslaHolder> CAPABILITY_HOLDER = null;
+    public static Capability<ITeslaHolder> TESLA_HOLDER = null;
     
     @CapabilityInject(IFluidHandler.class)
     public static Capability<IFluidHandler> FLUID_HANDLER = null;
+    
+    @CapabilityInject(IEnergyStorage.class)
+    public static Capability<IEnergyStorage> FORGE_ENERGY_STORAGE = null;
 }
--- src/main/java/rreeggkk/nuclearsciences/common/util/CompatUtil.java
+++ src/main/java/rreeggkk/nuclearsciences/common/util/CompatUtil.java
@@ -1,6 +1,9 @@
 package rreeggkk.nuclearsciences.common.util;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -11,9 +14,11 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.common.Optional;
 import rreeggkk.nuclearsciences.common.energy.AEnergyContainer;
+import scala.actors.threadpool.Arrays;
 
 public class CompatUtil {
 	public static boolean isTeslaLoaded() {
@@ -32,9 +37,7 @@ public class CompatUtil {
 	 * @param energy the energy container
 	 */
 	public static void tryPushEnergyFrom(World world, BlockPos pos, AEnergyContainer energy) {
-		if (isTeslaLoaded()) {
-			pushEnergyFrom(world, pos, energy, EnumFacing.VALUES);
-		}
+		tryPushEnergyFrom(world, pos, energy, EnumFacing.VALUES);
 	}
 
 	/**
@@ -45,26 +48,35 @@ public class CompatUtil {
 	 * @param energy the energy container
 	 * @param directions the directions to try to push the energy into
 	 */
-	public static void tryPushEnergyFrom(World world, BlockPos pos, AEnergyContainer energy, EnumFacing[] directions) {
+	public static void tryPushEnergyFrom(World world, BlockPos pos, AEnergyContainer energy, EnumFacing... directions) {
+		@SuppressWarnings("unchecked")
+		List<EnumFacing> facings = new ArrayList<>(Arrays.asList(directions));
 		if (isTeslaLoaded()) {
-			pushEnergyFrom(world, pos, energy, directions);
+			pushTeslaEnergyFrom(world, pos, energy, facings);
+		} else {
+			pushForgeEnergyFrom(world, pos, energy, facings);
 		}
 	}
 
 	@Optional.Method(modid="tesla")
-	private static void pushEnergyFrom(World world, BlockPos pos, AEnergyContainer energy, EnumFacing[] directions) {
-		if (energy.getStoredPower() == 0) {
-			return;
-		}
-		Map<ITeslaConsumer, Long> facing = new HashMap<ITeslaConsumer, Long>(directions.length);
+	private static void pushTeslaEnergyFrom(World world, BlockPos pos, AEnergyContainer energy, List<EnumFacing> directions) {
+		if (energy.getStoredPower() == 0) return;
+		Map<ITeslaConsumer, Long> facing = new HashMap<>(directions.size());
 		long totalPower = 0;
-		for (EnumFacing dir : directions) {
-			TileEntity te = world.getTileEntity(pos.add(dir.getFrontOffsetX(), dir.getFrontOffsetY(), dir.getFrontOffsetZ()));
-			if (te != null && !te.isInvalid() && te.hasCapability(CapabilityUtil.CAPABILITY_CONSUMER, dir.getOpposite())) {
-				ITeslaConsumer cons = te.getCapability(CapabilityUtil.CAPABILITY_CONSUMER, dir.getOpposite());
+		Iterator<EnumFacing> iter = directions.iterator();
+		while (iter.hasNext()) {
+			EnumFacing dir = iter.next();
+			TileEntity te = world.getTileEntity(pos.offset(dir));
+			if (te != null && !te.isInvalid() && te.hasCapability(CapabilityUtil.TESLA_CONSUMER, dir.getOpposite())) {
+				ITeslaConsumer cons = te.getCapability(CapabilityUtil.TESLA_CONSUMER, dir.getOpposite());
 				long power = cons.givePower(energy.getStoredPower(), true);
 				if (power > 0) {
-					totalPower += power;
+					if (energy.getStoredPower()-totalPower >= power) {
+						totalPower += power;
+					} else {
+						totalPower = energy.getStoredPower();
+					}
+					iter.remove();
 					facing.put(cons, power);
 				}
 			}
@@ -73,6 +85,35 @@ public class CompatUtil {
 		for (Entry<ITeslaConsumer, Long> e : facing.entrySet()) {
 			long power = energy.takePower(e.getValue()*originalPower/totalPower, true);
 			energy.takePower(e.getKey().givePower(power, false), false);
+		}
+	}
+
+	private static void pushForgeEnergyFrom(World world, BlockPos pos, AEnergyContainer energy, List<EnumFacing> directions) {
+		if (energy.getEnergyStored() == 0) return;
+		Map<IEnergyStorage, Integer> facing = new HashMap<>(directions.size());
+		int totalPower = 0;
+		Iterator<EnumFacing> iter = directions.iterator();
+		while (iter.hasNext()) {
+			EnumFacing dir = iter.next();
+			TileEntity te = world.getTileEntity(pos.offset(dir));
+			if (te != null && !te.isInvalid() && te.hasCapability(CapabilityUtil.FORGE_ENERGY_STORAGE, dir.getOpposite())) {
+				IEnergyStorage cons = te.getCapability(CapabilityUtil.FORGE_ENERGY_STORAGE, dir.getOpposite());
+				int power = cons.receiveEnergy(energy.getEnergyStored(), true);
+				if (power > 0) {
+					if (energy.getMaxEnergyStored()-totalPower >= power) {
+						totalPower += power;
+					} else {
+						totalPower = energy.getEnergyStored();
+					}
+					iter.remove();
+					facing.put(cons, power);
+				}
+			}
+		}
+		int originalPower = energy.getEnergyStored();
+		for (Entry<IEnergyStorage, Integer> e : facing.entrySet()) {
+			int power = energy.extractEnergy(e.getValue()*originalPower/totalPower, true);
+			energy.extractEnergy(e.getKey().receiveEnergy(power, false), false);
 		}
 	}
 }
